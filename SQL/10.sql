08a014bb3a6d1df167000e5b803db0311a122281
Dmitry Moskalev <44023937+EnterSub@users.noreply.github.com>
2023-01-21 16:16:00 +0700

    Delete 10.sql

--- SQL/10.sql
+++
@@ -1,6 +0,0 @@
-select Product.model, price from Product
-join Printer
-on Product.model = Printer.model
-where price = (select max(price) from Printer)
-
--- Find the highest priced printer models. Output: model, price
